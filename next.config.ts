dc4d53695153850f71211e13f22aa98bf173aee0
Amayuru1999 <amarasingheau@gmail.com>
2026-05-12 14:44:39 +0530

    feat: Implement shared UI component inside user management

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
-
 const nextConfig: NextConfig = {
   output: 'standalone',
+  experimental: {
+    externalDir: true,
+  },
+  transpilePackages: ['rased-shared-ui'],
   async rewrites() {
     return {
       afterFiles: [
